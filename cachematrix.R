c357f7a19e89c6d8b1db8e63df56fda72862ea6d
Arjun Venkatesh <arjun1508@gmail.com>
2016-08-02 23:40:49 -0700

    Second commit

--- cachematrix.R
+++ cachematrix.R
@@ -20,6 +20,10 @@
 ## 2. Find inverse using:
 ##    > cacheSolve(mc)
 
+
+## `makeCacheMatrix`: This function creates a special "matrix" object
+## that can cache its inverse.
+
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
@@ -35,7 +39,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## `cacheSolve`: This function computes the inverse of the special
+## "matrix" returned by `makeCacheMatrix` above. If the inverse has
+##  already been calculated (and the matrix has not changed), then
+## `cacheSolve` should retrieve the inverse from the cache.
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
